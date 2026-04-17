e08cfc464a9fa3032d9c925d8e456c1ec3b35f97
thissidemayur <thissidemayur@gmail.com>
2026-04-17 06:36:38 +0530

    feat: learn about input-output

---
+++ 1-prerequits/basic_of_cpp/input-output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h> //include almost all standered library. not recommend for production
+
+string input(){
+    string inp;
+    cin >> inp;
+    return inp;
+}
+
+int main(){
+    std::cout << "Namashkar duniya";
+    std::cout<<"\n new line"<<std::endl;
+    string inp = input();
+    cout<<"user Input"<< inp <<endl;
+    return 0;
+}
