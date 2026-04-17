cac641bdadd544a19034dd97a91ebd6b8b6de8d2
thissidemayur <thissidemayur@gmail.com>
2026-04-17 08:30:15 +0530

    feat: remmeber for loop syntax

---
+++ 1-prerequits/basic_of_cpp/for-loop.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void oneDArray() {
+     for (int i=1; i<=10;i++){
+        cout <<i <<endl;
+    }
+}
+
+void twoDArray() {
+    for (int i=0;i<3;i++ ){
+        for(int j=0;j<3;j++){
+            cout << i<<" "<<j << endl;
+        }
+    }
+}
+int main() {
+    // 1-D Array
+     oneDArray();
+   
+
+    // 2-D Array
+    twoDArray();
+
+}
