1ae6ef2f9a58ee376382ff2bcb72b0535d01144f
thissidemayur <thissidemayur@gmail.com>
2026-04-20 09:44:19 +0530

    feat: learn about recurssion printing

---
+++ 1-prerequits/basic_of_recurssion/5-factorial_of_num.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Sum of first N Natural Numbers
+
+
+Problem Statement: Given a number ‘N’, find out the sum of the first N natural numbers .
+
+Examples
+Input: N=5
+Output: 15
+Explanation: 1+2+3+4+5=15
+
+Input: N=6
+Output: 21
+Explanation: 1+2+3+4+5+6=15
+
+*/
+
+int factorial_of_numusing_recurssion(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    return num * factorial_of_numusing_recurssion(num - 1);
+}
+
+int main()
+{
+    cout << factorial_of_numusing_recurssion(10);
+    cout << endl;
+
+    cout << factorial_of_numusing_recurssion(5);
+    cout << endl;
+
+    cout << factorial_of_numusing_recurssion(100);
+    cout << endl;
+}
