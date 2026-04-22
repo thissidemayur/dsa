9cb4ff308ed9213d5594d9329671ba2ae6b390c6
thissidemayur <thissidemayur@gmail.com>
2026-04-22 07:44:06 +0530

    feat: revise selection sort

---
+++ 2-sorting/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        swap(arr[i], arr[min]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(int);
+
+    selectionSort(arr, n);
+}
