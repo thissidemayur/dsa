e068b6aae4abf4f5e68474c7ee1eb5714a3bcac2
thissidemayur <thissidemayur@gmail.com>
2026-04-29 05:28:45 +0530

    feat: solve max_Avg_Subaarray_of_size_k

---
+++ 3-Array/easy/max_avg_subarray.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+643. Maximum Average Subarray I
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+*/
+
+double findMaxAverage(vector<int> &arr, int k)
+{
+    int p1=0;
+    int p2=0;
+    double sum = 0;
+    double maxAvg = INT_MIN;
+
+    while(p2 <arr.size()){
+        sum+=arr[p2];
+        if(p2-p1+1 < k) {
+            p2++;
+        }else if (p2-p1+1==k){
+            double avg = sum/k;
+            maxAvg = max(maxAvg,avg);
+            sum -= arr[p1];
+            p1++;
+            p2++;
+        }
+    }
+    return maxAvg;
+
+}
+
+
+int main() {
+    vector<int>arr1 = {1,12,-5,-6,50,3};
+
+    double ans = findMaxAverage(arr1,4);
+    cout << "ANS: "<< ans<<endl;
+    return 0;
+}
